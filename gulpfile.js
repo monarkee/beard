2b122668c23b46271354f7f4f033469bc29b3647
David Hemphill <davidlee.hemphill@gmail.com>
2015-03-07 16:23:18 -0600

    Beard 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,33 @@
 // Gulp Requires
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    notify = require('gulp-notify'),
     plumber = require('gulp-plumber'),
-    amaretto = require('amaretto'),
     autoprefixer = require('gulp-autoprefixer'),
-    scss = require('gulp-ruby-sass'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    clean = require('gulp-clean'),
-    concat = require('gulp-concat');
-
-// Node requires for exec and sys
-var exec = require('child_process').exec,
-    sys = require('sys');
+    sass = require('gulp-sass'),
+    minifycss = require('gulp-minify-css');
 
 // Directories
 var DIST = 'dist/';
 
+// Error handler for Plumber
 var onError = function (err) {
     gutil.beep();
 };
 
 // SCSS Compiling and Minification
-gulp.task('scss', function() {
+gulp.task('sass', function() {
     return gulp.src('beard.scss')
         .pipe(plumber({
             errorHandler: onError
         }))
-        .pipe(scss({
-            loadPath: [amaretto.includePaths],
+        .pipe(sass({
             debugInfo: false,
             lineNumbers: false
         }))
         .pipe(autoprefixer('last 2 version'))
-        // .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
         .pipe(gulp.dest(DIST));
-        //.pipe(notify({message: 'Beard: scss compiled.'}));
-});
-
-/**
- * Watch Files For Changes
- */
-gulp.task('watch', function () {
-    gulp.watch('beard.scss', ['scss']);
 });
 
 // Gulp Default Task
-gulp.task('default', ['scss', 'watch'])
+gulp.task('default', ['sass']);
