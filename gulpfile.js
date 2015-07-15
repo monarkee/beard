80475abb065fcdb59519ce15f0547fc9632f0ab7
David Hemphill <davidlee.hemphill@gmail.com>
2015-07-14 22:21:34 -0500

    fix media queries fo shizzle

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     autoprefixer = require('gulp-autoprefixer'),
+    plumber = require('gulp-plumber');
     sass = require('gulp-sass'),
     minifycss = require('gulp-minify-css');
 
 // Directories
 var dist = 'dist';
+var sitedist = 'source/css';
 var paths = {
     css: ['./beard.scss', 'stylesheets/**/*.scss']
 }
@@ -14,12 +16,14 @@ var paths = {
 // SCSS Compiling and Minification
 gulp.task('sass', function() {
     return gulp.src('beard.scss')
+        .pipe(plumber())
         .pipe(sass({
             debugInfo: false,
             lineNumbers: false
         }))
         .pipe(autoprefixer('last 2 version'))
         .pipe(minifycss())
+        .pipe(gulp.dest(sitedist))
         .pipe(gulp.dest(dist));
 });
 
