1978053711dabd3cd59249032e5c3c2bc77b9290
David Hemphill <davidlee.hemphill@gmail.com>
2016-06-25 10:39:20 -0500

    Update nav to be a little more clear

--- source/_partials/_sidebar.blade.php
+++ source/_partials/_sidebar.blade.php
@@ -8,7 +8,7 @@
             <li class="ft4 pv1 brdr1--bottom bcg10">
                 <a href="/configuration" class="tc1 dim">Configuration</a>
             </li>
-            <li class="ft4 pv1">
+            <li class="ft4 pv1 brdr1--bottom bcg10">
                 <a href="/philosophy" class="tc1 dim">Philosophy</a>
             </li>
         </ul>
@@ -21,33 +21,30 @@
             <li class="ft4 pv1 brdr1--bottom bcg10">
                 <a href="/responsive-prefixes" class="tc1 dim lh1-5">Responsive Prefixes</a>
             </li>
-            <li class="ft4 pv1 brdr1--bottom bcg10">
-                <a class="tcg50">Helpers</a>
-                <ul class="mt1">
-                    <li class="ft4 mb1"><a href="/borders" class="tcg40 dim">Borders</a></li>
-                    <li class="ft4 mb1"><a href="/border-radius" class="tcg40 dim">Border Radius</a></li>
-                    <li class="ft4 mb1"><a href="/colors" class="tcg40 dim">Colors</a></li>
-                    <li class="ft4 mb1"><a href="/display" class="tcg40 dim">Display</a></li>
-                    <li class="ft4 mb1"><a href="/flex" class="tcg40 dim">Flex</a></li>
-                    <li class="ft4 mb1"><a href="/floats" class="tcg40 dim">Floats</a></li>
-                    <li class="ft4 mb1"><a href="/fonts" class="tcg40 dim">Fonts</a></li>
-                    <li class="ft4 mb1"><a href="/grid" class="tcg40 dim">Grid</a></li>
-                    <li class="ft4 mb1"><a href="/letter-spacing" class="tcg40 dim">Letter Spacing</a></li>
-                    <li class="ft4 mb1"><a href="/line-heights" class="tcg40 dim">Line heights</a></li>
-                    <li class="ft4 mb1"><a href="/misc" class="tcg40 dim">Misc</a></li>
-                    <li class="ft4 mb1"><a href="/overflow" class="tcg40 dim">Overflow</a></li>
-                    <li class="ft4 mb1"><a href="/position" class="tcg40 dim">Position</a></li>
-                    <li class="ft4 mb1"><a href="/spacing" class="tcg40 dim">Spacing</a></li>
-                    <li class="ft4 mb1"><a href="/text-alignment" class="tcg40 dim">Text alignment</a></li>
-                </ul>
-            </li>
-            <li class="ft4 pv1">
-                <a href="/tools" class="tc1 dim">Tools</a>
-                <ul class="mt1">
-                    <li class="ft4 mb1"><a href="/tools#appearance" class="tcg40 dim">appearance()</a></li>
-                    <li class="ft4 mb1"><a href="/tools#font-size" class="tcg40 dim">font-size()</a></li>
-                </ul>
-            </li>
         </ul>
-    </div>
+
+        <h2 class="ft6 pv1 tcg60 fw3">Helpers</h2>
+        <ul class="mb3">
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/borders" class="tc1 dim lh1-5">Borders</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/border-radius" class="tc1 dim lh1-5">Border Radius</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/colors" class="tc1 dim lh1-5">Colors</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/display" class="tc1 dim lh1-5">Display</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/flex" class="tc1 dim lh1-5">Flex</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/floats" class="tc1 dim lh1-5">Floats</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/fonts" class="tc1 dim lh1-5">Fonts</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/grid" class="tc1 dim lh1-5">Grid</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/letter-spacing" class="tc1 dim lh1-5">Letter Spacing</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/line-heights" class="tc1 dim lh1-5">Line heights</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/misc" class="tc1 dim lh1-5">Misc</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/overflow" class="tc1 dim lh1-5">Overflow</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/position" class="tc1 dim lh1-5">Position</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/spacing" class="tc1 dim lh1-5">Spacing</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/text-alignment" class="tc1 dim lh1-5">Text alignment</a></li>
+        </ul>
+
+        <h2 class="ft6 pv1 tcg60 fw3">Tools</h2>
+        <ul class="mb3">
+        <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/tools#appearance" class="tc1 dim lh1-5">appearance()</a></li>
+            <li class="ft4 pv1 brdr1--bottom bcg10"><a href="/tools#font-size" class="tc1 dim lh1-5">font-size()</a></li>
+        </div>
 </aside>
